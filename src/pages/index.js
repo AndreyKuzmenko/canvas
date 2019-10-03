cc5f0dbab4e19f9d645a3a1ce8f3d428776c0be3
Andrey Kuzmenko <Andrey.Kuzmenko@materialise.kiev.ua>
2019-10-03 11:14:21 +0300

    trying to fix css in gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import AppBar from '@material-ui/core/AppBar';
@@ -63,8 +63,8 @@ const styles = theme => ({
     padding: theme.spacing.unit * 6,
   },
 });
-
-
+*/
+/*
 function Home(props) {
   const { classes } = props;
  
@@ -75,7 +75,7 @@ function Home(props) {
 
       <main>
         <div className={classNames(classes.layout, classes.cardGrid)}>
-          {/* End hero unit */}
+
           <Grid container spacing={40}>
 
               <Grid item sm={6} md={4} lg={3}>
@@ -137,5 +137,115 @@ Home.propTypes = {
 };
 
 export default withStyles(styles)(Home);
-
+*/
 //file:///D:/GoogleDrive/Book/%D0%A1%D0%B0%D0%BC%D0%BE%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D0%B5/%D0%9A%D0%90%D0%9A%20%D0%A0%D0%90%D0%97%D0%9E%D0%91%D0%A0%D0%90%D0%A2%D0%AC%D0%A1%D0%AF%20%D0%A1%20%D0%A5%D0%90%D0%9E%D0%A1%D0%9E%D0%9C%20%D0%92%20%D0%93%D0%9E%D0%9B%D0%9E%D0%92%D0%95.pdf
+
+
+
+
+
+
+
+
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+
+
+
+const styles = theme => ({
+  cardGrid: {
+
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  }
+});
+
+
+function IndexPage(props) {
+ 
+  const { classes } = props;
+  return (
+
+  <Layout>
+    <SEO title="Home" />
+    <div >
+
+    <Grid container spacing={40}>
+
+<Grid item sm={6} md={4} lg={3}>
+  <Card className={classes.card}>
+    <CardActionArea href="/leadership">
+      <CardMedia
+        className={classes.cardMedia}
+        image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+        title="Image title"
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+        Leadership styles
+        </Typography>
+        <Typography>
+              Helps you decide on and prioritize tasks by urgency and importance, sorting out less urgent and important tasks which you should either delegate or not do at all.
+        </Typography>
+      </CardContent>
+      
+    </CardActionArea>
+  </Card>
+
+</Grid>
+
+<Grid item sm={6} md={4} lg={3}>
+  <Card className={classes.card}>
+    <CardActionArea href="/priorities">
+      <CardMedia
+        className={classes.cardMedia}
+        image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+        title="Image title"
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+        Eisenhower Matrix
+        </Typography>
+        <Typography>
+        helps you decide on and prioritize tasks by urgency and importance, sorting out less urgent and important tasks which you should either delegate or not do at all.
+        </Typography>
+      </CardContent>
+      
+    </CardActionArea>
+  </Card>
+
+
+</Grid>
+
+
+</Grid>
+
+</div>
+ 
+  </Layout>
+  );
+}
+
+export default withStyles(styles)(IndexPage);
